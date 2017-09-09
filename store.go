bb72fde005e753c974017bf612aee607df8b3816
Nick Heyn <nick@heyn.io>
2017-09-08 22:01:04 -0600

    Add the .Dispatch(...) method to the Store type.

--- store.go
+++ store.go
@@ -2,13 +2,26 @@ package store
 
 // A Store keeps track of data in a State, and "attempts to make state mutations predictable".
 type Store struct {
-	state *State
+	state State
 }
 
-func New(initialState State) Store {
-	st := Store{
-		state: &initialState,
+// Creates a new Store that start with the given state.
+func New(initialState State) *Store {
+	st := &Store{
+		state: initialState,
 	}
 
 	return st
+}
+
+// Dispatches the given action to all of the Updaters in the state of the Store. If an error is
+// returned, then the State will not not change (even for the Updaters that had already completed).
+func (s *Store) Dispatch(action interface{}) error {
+	newState, err := performUpdates(s.state, action)
+	if err != nil {
+		return err
+	}
+
+	s.state = newState
+	return nil
 }
--- store_test.go
+++ store_test.go
@@ -10,7 +10,7 @@ func TestGivenInitialState(t *testing.T) {
 	}
 
 	st := New(initialState)
-	currState := *st.state
+	currState := st.state
 
 	if len(initialState) != len(currState) {
 		t.Error("State from the Store has ", len(initialState), " Updaters and the intial state has ", len(currState))
@@ -24,6 +24,36 @@ func TestGivenInitialState(t *testing.T) {
 
 		if data != initialData {
 			t.Error("Inncorrect data the State in the Store in Updater ", key)
+		}
+	}
+}
+
+func TestStoreWillCallUpdate(t *testing.T) {
+	state := State{
+		"Updater 0": testUpdater{false, nil},
+		"Updater 1": testUpdater{false, nil},
+		"Updater 2": testUpdater{false, nil},
+	}
+
+	st := New(state)
+
+	testAction := "Test action"
+	err := st.Dispatch(testAction)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	updatedState := st.state
+
+	for key, data := range updatedState {
+		testData := data.(testUpdater)
+
+		if !testData.didUpdate {
+			t.Error("Update method not called on", key)
+		}
+
+		if testData.action != testAction {
+			t.Error("Update method called with incorrect action, ", testData.action, ", on ", key)
 		}
 	}
 }
