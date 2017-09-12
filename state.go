3e093879e09c49348a8bf1ce6db4987489a4ef3a
Nick Heyn <nick@heyn.io>
2017-09-11 22:11:37 -0600

    Allow the Updaters to be updated concurrently.

--- state.go
+++ state.go
@@ -12,15 +12,38 @@ func (selSt *State) SelectFrom(currSt *State) {
 
 // Gets the updated version of the updaters in the given state, after the action is performed on them.
 func performUpdates(s State, action interface{}) (State, error) {
+	updateChan := make(chan keyedData, len(s))
+	errChan := make(chan error, len(s))
+	for key, data := range s {
+		go performUpdate(keyedData{key, data}, action, updateChan, errChan)
+	}
+
 	newState := State{}
-	for name, data := range s {
-		newData, err := data.Update(action)
-		if err != nil {
+	for len(newState) != len(s) {
+		select {
+		case update := <-updateChan:
+			newState[update.key] = update.data
+		case err := <-errChan:
 			return nil, err
 		}
-
-		newState[name] = newData
 	}
 
 	return newState, nil
+}
+
+// A data Updater from a State, with it's assigned key.
+type keyedData struct {
+	key  interface{}
+	data Updater
+}
+
+// Gets the updated version of the given updater.
+func performUpdate(in keyedData, action interface{}, outChan chan<- keyedData, errChan chan<- error) {
+	newData, err := in.data.Update(action)
+	if err != nil {
+		errChan <- err
+		return
+	}
+
+	outChan <- keyedData{key: in.key, data: newData}
 }
