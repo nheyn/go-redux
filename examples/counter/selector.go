258c12476a4a3fdc8f9104fed1b75fedbddce35a
Nick Heyn <nick@heyn.io>
2017-09-16 17:24:10 -0600

    Add basic counter example.

---
+++ examples/counter/selector.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/nheyn/store"
+
+// Selector
+type countInfo struct {
+  value int
+  isPositive bool
+}
+
+func (c *countInfo) SelectFrom(s *store.State) {
+  counterData := (*s)["COUNTER_STATE"].(counter)
+
+  c.value = int(counterData)
+  c.isPositive = counterData > 0
+}
+
+func (c *countInfo) isNegitive() bool {
+  return c.value < 0
+}
---
+++ examples/counter/updater.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "context"
+  "errors"
+  "github.com/nheyn/store"
+)
+
+// Updater
+type counter int
+
+func (c counter) Update(_ context.Context, action interface{}) (store.Updater, error) {
+  switch amount := action.(type) {
+  case incurment:
+    if amount < 0 {
+      return nil, errors.New("Do not use negitive incurment actions, use decurment instead")
+    }
+
+    return c + counter(amount), nil
+  case decurment:
+    if amount < 0 {
+      return nil, errors.New("Do not use negitive decurment actions, use incurment instead")
+    }
+
+    return c - counter(amount), nil
+  default:
+    return c, nil
+  }
+}
+
+// Actions
+type incurment int
+
+type decurment int
