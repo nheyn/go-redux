49ca4aef5b07ac87edfa699315630a996038fbb9
Nick Heyn <nick@heyn.io>
2017-09-16 14:58:45 -0600

    Add an error test for the State.

--- state_test.go
+++ state_test.go
@@ -33,6 +33,26 @@ func TestStateWillCallUpdate(t *testing.T) {
 	}
 }
 
+func TestStateCanErrorDuringUpdate(t *testing.T) {
+	state := State{
+		"Updater 0": testUpdater{},
+		"Updater 1": testUpdaterError{},
+		"Updater 2": testUpdater{},
+	}
+
+	testAction := "Test action"
+	updatedState, err := performUpdates(context.Background(), state, testAction)
+	if err == nil {
+		t.Fatal("Updater should have retuned an error")
+		return
+	}
+
+	if updatedState != nil {
+		t.Fatal("Updater should not have retuned an updater")
+		return
+	}
+}
+
 type testUpdater struct {
 	name    string
 	actions []interface{}
@@ -51,4 +71,29 @@ func (u testUpdater) Update(_ context.Context, action interface{}) (Updater, err
 
 func (u testUpdater) didUpdate() bool {
 	return u.actions != nil && len(u.actions) > 0
+}
+
+type testUpdaterError struct {
+	errorOn int
+	actions []interface{}
+}
+
+func (u testUpdaterError) Update(_ context.Context, action interface{}) (Updater, error) {
+	var newActions []interface{}
+	if u.actions == nil {
+		newActions = []interface{}{action}
+	} else {
+		newActions = append(u.actions, action)
+	}
+
+	newU := testUpdaterError{u.errorOn, newActions}
+	if len(newU.actions)-1 == u.errorOn {
+		return nil, u
+	}
+
+	return u, nil
+}
+
+func (u testUpdaterError) Error() string {
+	return "The testUpdaterError correctly return this error"
 }
